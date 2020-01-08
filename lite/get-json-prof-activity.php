541d882e2a15864f61d169a6199f080fdd09fca4
Kanokkorn chompuhom <kanokkorn17457@gmail.com>
2020-01-08 22:28:17 +0700

    prof check request portfolio

--- lite/get-json-prof-activity.php
+++ lite/get-json-prof-activity.php
@@ -7,7 +7,7 @@ $return_arr = array();
 $query = "SELECT P.id AS id,P.timestamp AS regdate, S.firstname AS firstname, S.lastname AS lastname, S.std_id AS stdid, A.name AS actname, ST.name AS statusname
 FROM portfolio AS P 
 LEFT JOIN student AS S ON P.std_id = S.id
-LEFT JOIN activity AS A ON P.act_id = A.id 
+LEFT JOIN activity_new AS A ON P.act_id = A.id 
 LEFT JOIN portfolio_status AS ST ON P.status_id = ST.id 
 ORDER BY P.id";
 
